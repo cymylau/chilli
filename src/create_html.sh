257eb7bcd62a3ac3ae2afbf00a8ddc9646f36ed1
Richard Johnson <richjjoh@gmail.com>
2021-12-05 13:44:37 +0000

    Tidied up ready for publishing

--- src/create_html.sh
+++ src/create_html.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 awk '
 BEGIN{
-    print "<!DOCTYPE html>\n\n<html>\n<style>\ntable {\nfont-family: arial, sans-serif;\nborder-collapse: collapse;\nwidth: 100%;\n}\ntd, th {\nborder: 1px solid #dddddd;\ntext-align: center;\npadding: 30px;\n}\ntr:nth-child(even) {\nbackground-color: #dddddd;\n}\n</style>\n<body>\n<table><tr><th>File Name</th><th>MD5</th><th>SHA1</th><th>SHA256</th></tr>"
+    print "<!DOCTYPE html>\n<html>\n<style>\ntable {\nfont-family: arial, sans-serif;\nborder-collapse: collapse;\nwidth: 100%;\n}\ntd, th {\nborder: 1px solid #dddddd;\ntext-align: center;\npadding: 30px;\n}\ntr:nth-child(even) {\nbackground-color: #dddddd;\n}\n</style>\n<body>\n<table><tr><th>File Name</th><th>MD5</th><th>SHA1</th><th>SHA256</th></tr>"
     }
     {print "<tr>"
     for(i=1;i<=NF;i++)
